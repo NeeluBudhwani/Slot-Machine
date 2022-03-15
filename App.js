46d2c61632bfcae4ec0cdc08625c2c8606ea2083
NeeluBudhwani <100606983+NeeluBudhwani@users.noreply.github.com>
2022-03-15 13:09:40 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,7 @@
 import React from "react";
 import "./index.css";
+import SlotM from "./SlotMach";
 
-const SlotM =(props) => {
-  //let x = '😄';
-  //let y = '😄';
-  //let z = '👍';
-  let {x , y , z} = props;
-  if(x===y && y===z){
-return(
-  <>
-    <div className="slot_inner">
-      <h1>{x} {y} {z}</h1>
-      <h1>This is Matching...</h1>
-      <hr />
-
-    </div>
-  </>
-)
-  }
-  else{
-    return(
-      <>
-        <div className="slot_inner">
-          <h1>{x} {y} {z}</h1>
-          <h1>This is Not Matching...</h1>
-          <hr />
-    
-        </div>
-      </>
-    )
-
-  }
-
-}
 
 
 const App =() =>{
